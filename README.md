7b6de9d7e2bc3868c2d8f0b81e6798678859fd0d
pingo <--get>
2019-02-22 12:56:57 -0500

    Add the form.

--- README.md
+++ README.md
@@ -4,3 +4,4 @@ synesthesia-html
 Touch a color in a image uploaded via html form 
 and listen to the color sound.
 
+https://josepplloo.github.io/synesthesia-html/
--- script.js
+++ script.js
@@ -2,9 +2,17 @@ console.log("Hola");
 
 function paintForm () {
   const formSection = document.querySelector('.form-section');
-
+  const domForm = `<input
+   id="myFileInput" 
+   type="file" 
+   accept="image/*;capture=camera">
+  `
+  formSection.innerHTML = domForm;
 }
 
 function builApp() {
-  
+  paintForm();
+
 }
+
+builApp();
